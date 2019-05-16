459884e2b8100b373788da8f68a89e15dc51ca72
tallan groberg <tallan.taven@gmail.com>
2019-05-15 22:11:55 -0600

    more controller config

--- app/controllers/depts_controller.rb
+++ app/controllers/depts_controller.rb
@@ -1,13 +1,43 @@
 class DeptsController < ApplicationController
+  before_action :set_shop
+   before_action :set_dept, only: [:show]
+
+
   def index
+    @depts = @shop.depts
   end
 
   def show
   end
 
   def new
+    @dept = @shop.depts.new
+    render partial: "form"
+  end
+  def create
+    @dept = @shop.depts.new(@dept_params)
+    if @dept.save
+      redirect_to shop_depts_path(@shop.id, @dept.id)
+    else
+      render partial: "form"
+    end
   end
 
   def edit
+  end
+
+  private
+
+  def set_shop
+    @shop = Shop.find(params[:shop_id])
+
+  end
+  def set_dept
+    @dept = @shop.depts(params[:id])
+
+  end
+  def dept_params
+    params.require(:dept).permit(:name, :body)
+
   end
 end
--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,13 +1,58 @@
 class ShopsController < ApplicationController
-  def index
-  end
+before_action :set_shop, only: [:show, :edit, :update, :destroy]
 
-  def show
-  end
+before_action :set_shop, only: [:show, :edit, :update, :destroy]
+ def index
+   @shops = Shop.all
+   # render: :index
+ end
 
-  def new
-  end
+ def show
 
-  def edit
-  end
+ end
+
+ def new
+   #render :new
+   @shop = Shop.new
+   render partial: "form"
+ end
+ def edit
+   render partial: "form"
+ end
+ def update
+
+   if @shop.update(shop_params)
+     redirect_to shop_path(@shop.id)
+   else
+     render :edit
+end
+ end
+ def create
+   @shop = Shop.new(shop_params)
+
+   if @shop.save
+     # redirect_to shop_path(@shop) same as below
+     redirect_to @shop
+   else
+     render :new
+   end
+ end
+
+ def destroy
+   @shop.destroy
+   redirect_to shops_path
+
+ end
+
+
+ private
+
+ def set_shop
+   @shop = Shop.find(params[:id])
+
+ end
+   def shop_params
+     #params = { shop: { name: "shop Name"}}
+     params.require(:shop).permit(:name)
+   end
 end
